836e9713ca1c51d6ebbf6a27b777f07ed4e80c1e
MansoorAhmadQazi <mansoorqazi71@gmail.com>
2025-03-02 23:09:19 +0500

    working on routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('order_form');
 });
+
+Route::get('/admin/settings', function () {
+    return view('admin.settings');
+})->middleware('auth');
+
+Route::post('/admin/settings/update', [AdminController::class, 'updateSettings'])->middleware('auth');
